4770805a73c32acce3186930980dec095d8a0bde
olhakostovetska <kostovetskaolha@gmail.com>
2025-05-24 23:44:37 +0300

    solution3

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,9 +1,9 @@
 describe('article', () => {
-  // Генерація унікального користувача
+  // Генеруємо унікального користувача для кожного тесту
   function generateUser() {
     const randomNumber = Math.random().toString().slice(2);
     const userName = `test_mate_user${randomNumber}`;
-    const email = userName + '@mail.com';
+    const email = `${userName}@mail.com`;
     const password = 'Test1234!';
     return { userName, email, password };
   }
@@ -19,15 +19,17 @@ describe('article', () => {
     description = `${userName} description`;
     body = `${userName} body`;
 
-    cy.login(email, userName, password);
+    cy.login(email, userName, password); // логін перед кожним тестом
   });
 
   it('Should create an article', () => {
     cy.visit('https://conduit.mate.academy/editor');
+
     cy.get('[placeholder="Article Title"]').type(title);
     cy.get('[placeholder="What\'s this article about?"]').type(description);
     cy.get('[placeholder="Write your article (in markdown)"]').type(body);
-    cy.get('[type="button"]').click();
+
+    cy.get('[type="button"]').click(); // публікація статті
 
     cy.get('h1').should('contain.text', title);
     cy.get('div > p').should('contain.text', body);
@@ -40,7 +42,9 @@ describe('article', () => {
       cy.visit(`https://conduit.mate.academy/article/${slug}`);
 
       cy.contains('.btn', 'Delete Article').click();
+
       cy.contains('.nav-link', 'Global Feed').should('be.visible');
+
       cy.get('.article-preview')
         .should('contain.text', 'No articles are here... yet.');
     });
